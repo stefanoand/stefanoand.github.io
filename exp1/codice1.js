b375beef514df3fcd37c6b3146c2bc28c5b4f4e0
stefanoand <94043621+stefanoand@users.noreply.github.com>
2021-11-17 10:04:42 +0100

    Update codice1.js

--- exp1/codice1.js
+++ exp1/codice1.js
@@ -1,21 +1,21 @@
-define(['pipAPI', 'https://stefanoand.github.io/exp1/codice2.js'], function(APIConstructor, stiatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
 	
 	var API = new APIConstructor();
 		  return stiatExtension({
 		  category : { 
-		    name : 'Femminismo', //Will appear in the data.
+		    name : 'Black people', //Will appear in the data.
 		    title : {
-		      media : {word : 'Femminismo'}, //Name of the category presented in the task.
+		      media : {word : 'Black people'}, //Name of the category presented in the task.
 		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
 		      height : 7 //Used to position the "Or" in the combined block.
 		    }, 
 		    media : [ //Stimuli content as PIP's media objects
-    		    	{image : 'paritadigenere.jpg'}, 
-    			{image : 'paritadigenere.jpg'}, 
-    			{image : 'paritadigenere.jpg'}, 
-    			{image : 'paritadigenere.jpg'}, 
-    			{image : 'paritadigenere.jpg'}, 
-    			{image : 'paritadigenere.jpg'}
+    		    	{image : 'black1.jpg'}, 
+    			{image : 'black2.jpg'}, 
+    			{image : 'black3.jpg'}, 
+    			{image : 'black4.jpg'}, 
+    			{image : 'black5.jpg'}, 
+    			{image : 'black6.jpg'}
 		    ], 
 		    //Stimulus css (style)
 		    css : {color:'#31b404','font-size':'3em'}
@@ -23,45 +23,45 @@ define(['pipAPI', 'https://stefanoand.github.io/exp1/codice2.js'], function(APIC
 
   		attribute1 : 
 			{
-			name : 'Spiacevole', //Attribute label
+			name : 'Unpleasant', //Attribute label
 			title : {
-				media : {word : 'Negativo'}, //Name of the category presented in the task.
+				media : {word : 'Negative'}, //Name of the category presented in the task.
 				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
 				height : 7 //Used to position the "Or" in the combined block.
 			}, 
 			media : [ //Stimuli
-				{word: 'Odio'},
-				{word: 'Antipatia'},
-				{word: 'Rabbia'},
-				{word: 'Disastro'},
-				{word: 'Schifo'},
-				{word: 'Omicidio'}
+				{word: 'Bomb'},
+				{word: 'Abuse'},
+				{word: 'Sadness'},
+				{word: 'Pain'},
+				{word: 'Poison'},
+				{word: 'Grief'}
 			], 
 			//Can change color and size of the targets here.
 			css : {color:'#31b404','font-size':'3em'}
 			},
 		attribute2 : 
 			{
-			name : 'Piacevole', //Attribute label
+			name : 'Pleasant', //Attribute label
 			title : {
-				media : {word : 'Positivo'}, //Name of the category presented in the task.
+				media : {word : 'Positive'}, //Name of the category presented in the task.
 				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
 				height : 7 //Used to position the "Or" in the combined block.
 			}, 
 			media : [ //Stimuli
-				{word: 'Allegria'},
-				{word: 'Amore'},
-				{word: 'Felicita'},
-				{word: 'Simpatia'},
-				{word: 'Pace'},
-				{word: 'Paradiso'}
+				{word: 'Paradise'},
+				{word: 'Pleasure'},
+				{word: 'Cheer'},
+				{word: 'Wonderful'},
+				{word: 'Splendid'},
+				{word: 'Love'}
 			], 
 			//Can change color and size of the targets here.
 			css : {color:'#31b404','font-size':'3em'}
 			},
 
   base_url : {//Where are your images at?
-    image : 'https://stefanoand.github.io/exp1/img/'
+    image : 'https://baranan.github.io/minno-tasks/images/'
   }}
   );
   });
