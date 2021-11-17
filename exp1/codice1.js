4d55b6b75db74514733994fabe28514c2d45998d
stefanoand <94043621+stefanoand@users.noreply.github.com>
2021-11-17 09:35:28 +0100

    Update codice1.js

--- exp1/codice1.js
+++ exp1/codice1.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
+define(['pipAPI', 'https://stefanoand.github.js/exp1/codice2.js'], function(APIConstructor, stiatExtension){
 	
 	var API = new APIConstructor();
 		  return stiatExtension({
